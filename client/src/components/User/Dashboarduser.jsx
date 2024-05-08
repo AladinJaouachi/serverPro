022687b128259e7e54969aa8b31775a451fae16a
Aladin-Jaouachi <alaajawachi5@gmail.com>
2024-05-08 20:19:35 +0100

    changes

--- client/src/components/User/Dashboarduser.jsx
+++ client/src/components/User/Dashboarduser.jsx
@@ -293,7 +293,9 @@ const Dashboarduser = () => {
       </div>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container className="container">
-          <Navbar.Brand href="">app logo</Navbar.Brand>
+          <Navbar.Brand href="">
+            <img src={logo} alt="" />
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="">Home</Nav.Link>
             <Nav.Link href="">about</Nav.Link>
